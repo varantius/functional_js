6000ef5f1d21e3ab22f571e20bab1dacc695d28a
Vyacheslav Povelitsa <vyacheslav.povelitsa@flexbricks.com>
2021-11-03 16:48:58 +0200

    Some changes

--- composition.js
+++ composition.js
@@ -36,3 +36,49 @@ const withСompose = compose(upperCase,exclaim, repeat);
 console.log(withСompose("Сротал")); // СРОТАЛ! СРОТАЛ! СРОТАЛ!
 
 //////////
+
+
+
+// https://frontend-stuff.com/blog/composition/
+
+// const map = array => cb => array.map(cb);
+// const arr = [1, 2, 3, 4, 5];
+// const double = n => n * 2;
+//
+// const withArr = map(arr);
+//
+// console.log(withArr(double)); // [ 2, 4, 6, 8, 10 ]
+// console.log(withArr(n => n * 3)); // [ 3, 6, 9, 12, 15 ]
+
+
+console.log('============================================')
+
+const map = cb => {
+    console.log('callback: ', cb)
+    return function (array) {
+       console.log('array: ', array)
+       return array.map(cb);
+    }
+}
+
+
+
+const prop = key => {
+    console.log('key: ', key)
+    return function (obj) {
+        console.log('obj[key]: ', obj[key])
+        return obj[key];
+    }
+}
+
+const getName = prop("name");
+console.log('getName: ', getName)
+
+const people = [
+    { name: "Alex" },
+    { name: "Julia" },
+    { name: "Leo" },
+    { name: "Den" }
+];
+
+console.log(map(getName)(people)); // ["Alex", "Julia", "Leo", "Den"]
