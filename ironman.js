4d1d7b59eddddea2a772b7e07e68e48b54757cd5
varantius <povelitsa.slava@gmail.com>
2021-12-01 22:24:11 +0200

    commit

--- ironman.js
+++ ironman.js
@@ -53,3 +53,23 @@ const newArray = mapArray(
 
 console.log(newArray(splitVal(' ')("Я Железный человек")))
 
+//----------------------------------------------------------------------------------------------------------------------
+
+function newString(str) {
+    return str.split(' ').map(strVal => {
+        let result = strVal.toLowerCase().split('').reverse().join('')
+        if (strVal.charAt(0) === strVal.charAt(0).toUpperCase()) {
+            return result.charAt(0).toUpperCase() + result.slice(1)
+        }
+        return result
+    }).join(' ')
+}
+newString('Я Железный человек')
+
+
+console.time('test')
+for (var i = 0; i < 5000; i++){
+    newString('Принцесса на горошине Сидела Очень криво и тихо Тихо плакала сиреневая слива а Кот сидел на лавочке Листал свои Журналы и что еще Плавало такое В океане')
+}
+console.timeEnd('test')
+
