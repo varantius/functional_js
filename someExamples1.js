7a5a2af8dfb31d6d61c2b1b58e2168d0cd744232
varantius <povelitsa.slava@gmail.com>
2021-12-03 00:00:12 +0200

    merge examples

--- someExamples1.js
+++ someExamples1.js
@@ -25,3 +25,29 @@ let result = [...s].reduce((acc, char) => {
 }, {});
 debugger
 console.log(result); // {h: 1, e: 1, l: 2, o: 1}
+
+
+var test = [
+    {color: 'white', id: 3},
+    {color: 'black', id: 4},
+]
+
+const arrToObject = (arr) =>
+    arr.reduce((acc, item) => {
+        debugger
+        acc[item.color] = { ...acc[item.color], [item.id]: item };
+        return acc;
+    }, {});
+
+const outputObject = (array, output = {}) => {
+    array.forEach(el => {
+        // debugger
+        output[el.color] = {
+            [el.id]: el
+        }
+    });
+    return output;
+};
+
+console.log(arrToObject(test))
+console.log(outputObject(test))
